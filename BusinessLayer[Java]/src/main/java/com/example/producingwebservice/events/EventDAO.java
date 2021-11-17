d081da12560da88c16cf2396d606f4cfb74259b1
AsuraEyes <56295945+AsuraEyes@users.noreply.github.com>
2021-11-17 11:18:10 +0100

    added event dao

---
+++ BusinessLayer[Java]/src/main/java/com/example/producingwebservice/events/EventDAO.java
@@ -0,0 +1,35 @@
+package com.example.producingwebservice.events;
+
+import com.example.producingwebservice.db.DatabaseHelper;
+import io.spring.guides.gs_producing_web_service.Event;
+import io.spring.guides.gs_producing_web_service.EventList;
+
+import javax.annotation.Resource;
+
+public class EventDAO implements Events{
+    private DatabaseHelper<Event> helper;
+    private EventList eventList;
+
+    @Resource(name="jdbcUrl")
+    private String jdbcUrl;
+
+    @Resource(name="username")
+    private String username;
+
+    @Resource(name="password")
+    private String password;
+
+    public EventDAO() {
+        eventList = new EventList();
+    }
+
+    @Override
+    public EventList readByDate() {
+        return null;
+    }
+
+    @Override
+    public EventList readByCategory() {
+        return null;
+    }
+}
---
+++ BusinessLayer[Java]/src/main/java/com/example/producingwebservice/events/Events.java
@@ -0,0 +1,8 @@
+package com.example.producingwebservice.events;
+
+import io.spring.guides.gs_producing_web_service.EventList;
+
+public interface Events {
+    EventList readByDate();
+    EventList readByCategory();
+}
