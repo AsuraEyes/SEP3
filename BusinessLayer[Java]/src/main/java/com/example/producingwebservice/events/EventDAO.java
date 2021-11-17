57b4858816500988ea2309a3469b9ee4bb0217d6
AsuraEyes <56295945+AsuraEyes@users.noreply.github.com>
2021-11-17 12:00:40 +0100

    added filter by date and category

--- BusinessLayer[Java]/src/main/java/com/example/producingwebservice/events/EventDAO.java
+++ BusinessLayer[Java]/src/main/java/com/example/producingwebservice/events/EventDAO.java
@@ -1,37 +1,66 @@
 package com.example.producingwebservice.events;
 
+import com.example.producingwebservice.db.DataMapper;
 import com.example.producingwebservice.db.DatabaseHelper;
-import io.spring.guides.gs_producing_web_service.Event;
-import io.spring.guides.gs_producing_web_service.EventList;
+import io.spring.guides.gs_producing_web_service.*;
 
 import javax.annotation.Resource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
 
-public class EventDAO implements Events{
+public class EventDAO implements Events {
     private DatabaseHelper<Event> helper;
-    private EventList eventList;
+    private final EventList eventList;
 
-    @Resource(name="jdbcUrl")
+    @Resource(name = "jdbcUrl")
     private String jdbcUrl;
 
-    @Resource(name="username")
+    @Resource(name = "username")
     private String username;
 
-    @Resource(name="password")
+    @Resource(name = "password")
     private String password;
 
     public EventDAO() {
         eventList = new EventList();
     }
 
-    @Override
+    private static Event createEvent(int id, String name, String addressStreetName,
+                                     String addressStreetNumber, String addressApartmentNumber, int maxNumberOfParticipants,
+                                     int numberOfParticipants, String eventCategory, User organizer, UserList participants,
+                                     UserList organizers, EventGameList gameList) {
+        Event event = new Event();
+        event.setId(id);
+        event.setName(name);
+        event.setAddressStreetName(addressStreetName);
+        event.setAddressStreetNumber(addressStreetNumber);
+        event.setAddressApartmentNumber(addressApartmentNumber);
+        event.setMaxNumberOfParticipants(maxNumberOfParticipants);
+        event.setNumberOfParticipants(numberOfParticipants);
+        event.setEventCategory(eventCategory);
+        event.setOrganizer(organizer);
+        event.setParticipants(participants);
+        event.setOrganizers(organizers);
+        event.setGameList(gameList);
+        return event;
+    }
+
     public EventList readByDate() {
         eventList.getEventList().clear();
+        eventList.getEventList().addAll(helper.map(new EventMapper(), ""));
         return eventList;
     }
 
-    @Override
     public EventList readByCategory() {
         eventList.getEventList().clear();
+        eventList.getEventList().addAll(helper.map(new EventMapper(), ""));
         return eventList;
+    }
+
+    private static class EventMapper implements DataMapper<Event> {
+        public Event create(ResultSet rs) throws SQLException {
+            return null;
+        }
     }
 }
