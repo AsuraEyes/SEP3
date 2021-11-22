5b84612253215ec3b0ee68f2fe97bf20b9d33f03
143473 <75113646+143473@users.noreply.github.com>
2021-11-22 10:09:23 +0100

    created category endpoint, finished java part

--- BusinessLayer[Java]/src/main/java/com/example/producingwebservice/categories/Categories.java
+++ BusinessLayer[Java]/src/main/java/com/example/producingwebservice/categories/Categories.java
@@ -1,5 +1,8 @@
 package com.example.producingwebservice.categories;
 
+import io.spring.guides.gs_producing_web_service.CategoryList;
+
 public interface Categories
 {
+  CategoryList getCategories();
 }
