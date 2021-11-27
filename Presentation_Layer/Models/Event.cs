61ab0b4cd50c99f568573b7216863a0539350ce6
AsuraEyes <56295945+AsuraEyes@users.noreply.github.com>
2021-11-27 16:15:34 +0100

    fixed bugs

--- Presentation_Layer/Models/Event.cs
+++ Presentation_Layer/Models/Event.cs
@@ -1,5 +1,7 @@
+#nullable enable
 using System;
 using System.ComponentModel.DataAnnotations;
+using ExpressiveAnnotations.Attributes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation_Layer.Models
@@ -9,11 +11,31 @@ namespace Presentation_Layer.Models
         [Required]
         public int Id{ get; set; }
         
-        [Required]public string Name{ get; set; }
+        [Required]
+        public string Name{ get; set; }
+        
+        [Required]
+        [RequiredIf("Date <= Today")]
+        [AssertThat("Date > Today()", 
+            ErrorMessage = "The date must be later than today")]
+        public DateTime Date { get; set; }
+        
+        [Required]
+        [AssertThat("TimeStart > Now()", 
+            ErrorMessage = "The start time must be later than now.")]
+        public DateTime TimeStart { get; set; }
+        
+        [AssertThat("TimeEnd > TimeStart", 
+            ErrorMessage = "The end time must be later than the start time.")]
+        public DateTime TimeEnd;
 
         [Required]
+        [AssertThat("StarTime >= Today()")]
         public DateTime StartTime{ get; set; }
-
+        
+        [RequiredIf("EndTime <= Today()")]
+        [AssertThat("endTime > startTime", 
+            ErrorMessage = "The end time must be later than the start time.")] 
         public DateTime EndTime{ get; set; }
         
         [Required(ErrorMessage = "The Address Street Name is required.")]
@@ -22,7 +44,7 @@ namespace Presentation_Layer.Models
         [Required(ErrorMessage = "The Address Street Number is required.")]
         public string AddressStreetNumber{ get; set; }
         
-        public string AddressApartmentNumber{ get; set; }
+        public string? AddressApartmentNumber{ get; set; }
         public string Organizer { get; set; }
         
         [Required(ErrorMessage = "Max number of participants field is required.")]
@@ -31,7 +53,9 @@ namespace Presentation_Layer.Models
         
         public int NumberOfParticipants{ get; set; }
         
-        [Required]public int EventCategory{ get; set; }
+        [Required]
+        [AssertThat("EventCategory >= 1", ErrorMessage = "Select a category")]
+        public int EventCategory{ get; set; }
 
     }
 }
