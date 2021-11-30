abfd1d4f008919a0b8cdca0900f7b2b63522a4f1
Maggie <maki.lapcik@gmail.com>
2021-11-30 14:45:40 +0100

    xsd changed by SOAPEventGameListRequest Add and delete games from event game list - SQL and SOAP

--- BusinessLayer[Java]/src/main/java/com/example/producingwebservice/EventGameList/EventGameLists.java
+++ BusinessLayer[Java]/src/main/java/com/example/producingwebservice/EventGameList/EventGameLists.java
@@ -5,4 +5,6 @@ import io.spring.guides.gs_producing_web_service.GameList;
 public interface EventGameLists
 {
   GameList readAllEventGameList(int eventId);
+  void removeGameFromEventGameList(int gameId, int eventId, String username);
+  void addGameToEventGameList(int gameId, int eventId, String username);
 }
