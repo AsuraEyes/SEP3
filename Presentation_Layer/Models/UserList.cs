9980319c0cbe62063445af6337c9fb32e805fd83
AsuraEyes <56295945+AsuraEyes@users.noreply.github.com>
2021-12-03 08:49:45 +0100

    put input validation summary when creating and editing files

--- Presentation_Layer/Models/UserList.cs
+++ Presentation_Layer/Models/UserList.cs
@@ -3,11 +3,11 @@ namespace Presentation_Layer.Models
     public class UserList
     {
         public int Count { get; set; }
-        public User[] UserList { get; set; }
+        public User[] userList { get; set; }
 
         public UserList()
         {
-            UserList = new User[] { };
+            userList = new User[] { };
         }
     }
 }
