222fbdc6527b39096edd981d68d88e4af5318049
AsuraEyes <56295945+AsuraEyes@users.noreply.github.com>
2021-12-07 08:15:31 +0100

    made some unit tests

---
+++ UnitTests/GameTest.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Threading.Tasks;
+using BookAndPlaySOAP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLayer.Data;
+using BusinessLayer.Middlepoint;
+using BusinessLayer.Models;
+
+namespace BusinessTierTests
+{
+    [TestClass]
+    public class GameTest
+    {
+        private static readonly IGameWebService GameWebService = new GameWebService();
+        private readonly IGameMiddlePoint gameMiddlePoint = new GameMiddlePoint(GameWebService, null);
+
+        [TestMethod]
+        public async Task AddGameAsync()
+        {
+            try
+            {
+                var newGame = new Game
+                {
+                    name = "Go",
+                    complexity = 4,
+                    timeEstimation = 9,
+                    minNumberOfPlayers = 12
+                };
+                await gameMiddlePoint.AddGameAsync(newGame);
+            }
+            catch (AggregateException)
+            {
+                return;
+            }
+            Assert.Fail();
+        }
+    }
+}
