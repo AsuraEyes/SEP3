34863b69f37de84cb05259d58619f9f91bda5868
Maggie <maki.lapcik@gmail.com>
2021-12-08 13:57:52 +0100

    PaymentOverview.razor, Payment.razor css

--- PresentationTier/Models/EventList.cs
+++ PresentationTier/Models/EventList.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace PresentationTier.Models
 {
     public class EventList
     {
         public int Count { get; set; }
+        [JsonPropertyName("eventList")]
         public Event[] ListOfEvents { get; set; }
 
         public EventList()
